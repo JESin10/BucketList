df661d36b53f3a4a83a913bda47515ecb2f5290a
JESin10 <shinej1029@gmail.com>
2023-08-26 12:16:38 +0900

    [feat] : Login, Signup Page Setting (with Google)

--- src/page/Signup.tsx
+++ src/page/Signup.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useAuth } from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
-import { wait } from "@testing-library/user-event/dist/utils";
+import { useAuth } from "../context/AuthContext";
 import { loadImg } from "../assets/images";
 
-export default function Login() {
+export default function Signup() {
   const emailRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
-  const { login, signupWithGoogle, currentUser } = useAuth();
+  const passwordConfirmRef = useRef<HTMLInputElement | null>(null);
+  const { signup, signupWithGoogle, currentUser } = useAuth();
   const [error, setError] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const navigate = useNavigate();
@@ -18,23 +18,25 @@ export default function Login() {
     }
   });
 
-  // basic singup form
   const submitHandler = async (e: React.FormEvent) => {
     e.preventDefault();
+    if (passwordRef.current?.value !== passwordConfirmRef.current?.value) {
+      return setError("Password do not Match");
+    }
     try {
       setError("");
       setLoading(true);
       if (emailRef.current && passwordRef.current) {
-        await login(emailRef.current.value, passwordRef.current.value);
+        await signup(emailRef.current.value, passwordRef.current.value);
       }
       navigate("/");
     } catch (err) {
-      setError("Fail to sign up");
+      setError("Failed to create an account");
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
   };
 
-  //google signup form
   const SignupWithGoogleHandler = () => {
     setLoading(true);
     signupWithGoogle();
@@ -44,39 +46,43 @@ export default function Login() {
 
   return (
     <div>
-      <div className="flex mt-4">
-        <div className=" text-center bg-white md:w-4/12 w-full mx-auto shadow-sm p-10 px-12  rounded-2xl">
-          <h2 className="text-2xl font-semibold mb-6">Welcome back!</h2>
+      <div className="mt-4 flex items-center justify-center">
+        <div className="bg-white text-center w-full md:w-4/12 mx-auto p-10  shadow-sm rounded-2xl">
+          <h2 className="text-2xl font-semibold mb-4">Create your account</h2>
           {error && <p className="p-2 bg-red-100 m-4 mx-20">{error}</p>}
           <form
             onSubmit={submitHandler}
-            className="flex flex-col justify-center text-xl"
+            className="flex flex-col justify-center "
           >
             <input
               type="email"
               placeholder="Email"
               ref={emailRef}
-              className="border p-2 m-2 rounded"
-            ></input>
+              className="text-xl border p-2 m-2 rounded"
+            />
             <input
               type="password"
               placeholder="Password"
               ref={passwordRef}
-              className="border p-2 m-2 rounded"
-            ></input>
-
+              className="text-xl border p-2 m-2 rounded"
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              ref={passwordConfirmRef}
+              className="text-xl border p-2 m-2 rounded"
+            />
             <button
               disabled={loading}
-              className="font-semibold px-5 py-2 mx-auto rounded-full w-1/2 m-2 bg-indigo-500 text-white"
+              className="px-5 text-xl font-semibold py-2 mx-auto rounded-full w-1/2 m-4 bg-indigo-500 text-white"
             >
-              Log In
+              Sign up
             </button>
-            <div></div>
           </form>
 
           <button
             onClick={SignupWithGoogleHandler}
-            className="px-6 mx-auto border flex items-center justify-center shadow-sm rounded-full my-2 hover:bg-gray-100 p-2 text-xl"
+            className="mx-auto px-6 border flex items-center justify-center shadow-sm rounded-full my-2 hover:bg-gray-100 p-2 text-xl"
           >
             <img
               src={loadImg.googleLogo}
@@ -85,12 +91,14 @@ export default function Login() {
             />
             Continue with google
           </button>
-          <h2>
-            Need an account?{" "}
-            <Link to="/signup" className="text-yellow-300 font-semibold">
-              Sign Up
-            </Link>
-          </h2>
+          <div>
+            <h2>
+              Already a user?{" "}
+              <Link to="/login" className="text-yellow-400 font-semibold">
+                Log in
+              </Link>{" "}
+            </h2>
+          </div>
         </div>
       </div>
     </div>
