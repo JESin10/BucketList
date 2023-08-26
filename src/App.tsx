fd957bd5ae0fe82ea5ba1b9a854e2d3012f40514
JESin10 <shinej1029@gmail.com>
2023-08-26 12:02:10 +0900

    [feat] : Reacr Router Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React from "react";
-import "./App.css";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Loading from "./page/Loading";
+
+const Signup = lazy(() => import("./page/Signup"));
+const Login = lazy(() => import("./page/Login"));
+const Dashboard = lazy(() => import("./page/Dashboard"));
+const Explore = lazy(() => import("./page/Explore"));
+const PrivateRoute = lazy(() => import("./context/PrivateRoute"));
+const Navbar = lazy(() => import("./components/Navbar"));
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <AuthProvider>
+          <Suspense fallback={<Loading />}>
+            <Navbar />
+            {/* <PrivateRoute path="/" element={<Dashboard />} /> */}
+            <Routes>
+              {/* <Route
+                path="/"
+                element={<PrivateRoute element={<Dashboard />} />}
+              /> */}
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/explore" element={<Explore />} />
+              {/* <Route path="/notfound" element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} /> */}
+            </Routes>
+          </Suspense>
+        </AuthProvider>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
